c0c388ccacc7dd8fbd457f3e9a667a33d521f917
Alex <alex_ngo@live.com>
2015-05-11 00:53:17 -0700

    tabs component

---
+++ app/components/Tabs.jsx
@@ -0,0 +1,33 @@
+var Tabs = React.createClass({
+    getInitialState: function () {
+        return {
+            tab: 0
+        };
+    },
+    handleClick: function(tabValue){
+        this.setState({
+            tab: tabValue
+        });
+        this.props.handleTabClick(tabValue);
+    },
+    render: function(){
+        var currentTab = this.state.tab;
+        var getClass = function(tab) {
+            return classNames({
+            'active': currentTab === tab
+            });
+        }
+
+        return (
+            <div className="row">
+                <div className="col s12">
+                    <ul className="tabs">
+                    <li className="tab col s2"><a className={getClass(0)} onClick={this.handleClick.bind(null, 0)}>List</a></li>
+                        <li className="tab col s2"><a className={getClass(1)} onClick={this.handleClick.bind(null, 1)}>Graph</a></li>
+                        <li className="tab col s2"><a className={getClass(2)} onClick={this.handleClick.bind(null, 2)}>Map</a></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+});
