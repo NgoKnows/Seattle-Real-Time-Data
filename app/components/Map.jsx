75bc43e86a32d81111214f947e48ed0f378db2c0
Alex Ngo <alex_ngo@live.com>
2015-05-12 22:49:15 -0700

    Got new markers to drop rather than all markers

--- app/components/Map.jsx
+++ app/components/Map.jsx
@@ -1,19 +1,90 @@
 var Map = React.createClass({
+    getInitialState: function() {
+        return {
+            map: null,
+            markers : {}
+        }
+
+    },
     componentWillMount : function() {},
-    componentWillReceiveProps: function() {},
+    componentWillReceiveProps: function() {
+
+    },
     componentWillUnmount : function() {},
 
-    _parseData : function() {},
+    getMarkers : function() {
+        if(this.state.map) {
+            var events = this.props.events;
+            var markers = this.state.markers;
+            //if markers are being added
+            if (events.length > Object.keys(markers).length) {
+                for (var i = 0; i < events.length; i++) {
+                    var event = events[i];
+
+                    //if marker doesn't already exist
+                    if (!markers.hasOwnProperty(this.getKey(event))) {
+                        this.dropMarkers(this, event, i, markers);
+                    }
+                }
+            }else{
+                var newMarkers = {}
+                for (var i = 0; i < events.length; i++) {
+                    var event = events[i];
+                    var key = this.getKey(event);
+                    newMarkers[key] = markers[key];
+                    delete markers[key]
+                }
+                $.each(markers, function(index, value) {
+                    //for(var opacity = 1; opacity > 0.5; opacity -= .0001){
+                    //    value.setOpacity(opacity);
+                    //}
+                    value.setMap(null);
+                });
+                this.state.markers = newMarkers;
+
+            }
+        }
+    },
+
+    getKey: function(event){
+        //return event.latitude + event.longitude;
+        return event.key;
+    },
+
+    clearMarkers: function(){
+        for(var i = 0; i < this.state.markers.length; i++){
+            this.state.markers[i].setMap(null);
+        }
+    },
+
+    dropMarkers: function(component, event, i, markers){
+        window.setTimeout(function() {
+            markers[component.getKey(event)] = new google.maps.Marker({
+                position: new google.maps.LatLng(event.latitude, event.longitude),
+                map: component.state.map,
+                animation: google.maps.Animation.DROP,
+                title: event.event_clearance_subgroup,
+                optimized: false
+            });
+        }, i * 5);
+    },
+
     componentDidMount : function() {
         var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644)
+            zoom: 12,
+            center: new google.maps.LatLng(47.6097, -122.3331)
         };
-        map = new google.maps.Map(document.getElementById('map-canvas'),
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+
+        this.setState({
+                map: map
+            });
     },
 
     render: function() {
+        this.getMarkers();
+
         return (
         <div>
             <div id="map-canvas"></div>
@@ -23,5 +94,3 @@ var Map = React.createClass({
     }
 
 });
-
-React.render(<Map />, document.getElementById('mount'));
