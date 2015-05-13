fe9fc66b981447edc43413f707ca2c9f71e47f0f
Alex Ngo <alex_ngo@live.com>
2015-05-12 22:50:27 -0700

    test for transitiongroup

--- app/components/template.jsx
+++ app/components/template.jsx
@@ -1,18 +1,51 @@
-React.createClass({
+var List = React.createClass({
+        getInitialState: function() {
+                return {
+                        items: [0, 1, 2, 3]
+                };
+        },
 
-        propTypes: {},
-        mixins : [],
+        removeThenReadd: function(item) {
+                // {{{
+                var items = this.state.items.slice(0);
+                var found = false;
+                for(var i = 0; i < items.length; i++) {
+                        if (items[i] === item) {
+                                items.splice(i, 1);
+                                found = true;
+                                break;
+                        }
+                }
 
-        getInitialState: function() {},
-        getDefaultProps: function() {},
+                this.setState({items: items}, function() {
+                        if (found) {
+                                setTimeout(function() {
+                                        var items = this.state.items.slice(0);
+                                        items.push(item);
+                                        this.setState({items: items});
+                                }.bind(this), 5000);
 
-        componentWillMount : function() {},
-        componentWillReceiveProps: function() {},
-        componentWillUnmount : function() {},
+                        }
+                }.bind(this));
+                // }}}
+        },
 
-        _parseData : function() {},
-        _onSelect : function() {},
+        _makeDiv: function(index) {
+                // {{{
+                return <div onClick={this.removeThenReadd.bind(null, index)}
+                            style={itemStyle}
+                            key={index}>
+                        {"Item " + index}
+                </div>;
+                // }}}
+        },
 
-        render : function() {}
-
-    });
+        render: function() {
+                var items = this.state.items.map(this._makeDiv);
+                return <TimeoutTransitionGroup enterTimeout={500}
+                                               leaveTimeout={500}
+                                               transitionName="demo">
+                        {items}
+                </TimeoutTransitionGroup>;
+        }
+});
