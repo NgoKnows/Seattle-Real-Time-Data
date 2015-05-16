6bff4ff0c3dcff74d858e2278dfd7d5865c3735f
Alex <alex_ngo@live.com>
2015-05-16 11:28:14 -0700

    ajax call made within app now

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -7,20 +7,21 @@ var App = React.createClass({
         return {
             tab: 0,
             searchString: '',
-            events: this.props.events,
-            sortCategory: "recent",
-            recentValue: 500,
-            dateRange: [[]]
+            allEvents: [],
+            events: [],
+            recentValue: 100,
+            dateRange: [moment(-25, "DD"), moment()],
+            loading: false
         };
     },
 
-    handleCategorySwitch : function (category){
-        this.setState({
-            sortCategory: category
-        });
+    handleFilterClick: function(filter, value){
+        var filterObj = {}
+        filterObj[filter] = value;
+        this.state[filter] = value;
+        this.ajaxCall(filter);
     },
 
-    //handle
 
     handleTabClick: function(tabValue){
         this.setState({
@@ -36,7 +37,7 @@ var App = React.createClass({
     getFilteredEvents: function(){
         var searchString = this.state.searchString.toLowerCase();
 
-        var events = this.props.events.filter(function (event) {
+        var events = this.state.allEvents.filter(function (event) {
             return event.event_clearance_subgroup.toLowerCase().match(searchString);
         });
 
@@ -45,12 +46,55 @@ var App = React.createClass({
         });
     },
 
+    ajaxCall: function(filter){
+        var component = this;
+        this.setState({
+            loading : true
+        });
+        var url = "https://data.seattle.gov/resource/pu5n-trf4.json?";
+
+        if(filter === 'recentValue'){
+            url = "https://data.seattle.gov/resource/pu5n-trf4.json?$limit=" + this.state.recentValue +
+                "&$order=event_clearance_date DESC&$where=event_clearance_date > '2010-07-01'"
+        }
+        else{
+            var dateRange=  this.state.dateRange;
+            url = url + "$limit=5000&$order=event_clearance_date DESC&$where=event_clearance_date between " +
+                "'" + dateRange[0].format('YYYY-MM-DD') + "'" + ' and ' + "'" + dateRange[1].format('YYYY-MM-DD') + "'";
+            console.log(url);
+        }
+        $.ajax({
+            url: url,
+            type: "get"
+        }).done(function(data){
+            var i = 0;
+            data.forEach(function(item){
+                item.key = i++;
+            });
+            component.setState({
+                allEvents: data,
+                events: data
+            });
+            component.setState({
+                loading : false
+            })
+        });
+    },
+
+    componentDidMount: function(){
+        this.ajaxCall('recentValue');
+    },
     render : function() {
         return (
         <div>
+            {this.state.loading ?
+                <img id="ajaxLoader" src="assets/imgs/ellipsis.svg"/>
+            :null}
+
             <Tabs handleTabClick={this.handleTabClick}/>
             <Search handleSearchChange={this.handleSearchChange} />
-            <SortButtonGroup />
+            <SortButtonGroup recent={this.state.recentValue} dateRange={this.state.dateRange}
+                             handleFilterClick={this.handleFilterClick}/>
 
             {this.state.tab === 0 ?
             <EventTable events={this.state.events}/>
@@ -69,21 +113,4 @@ var App = React.createClass({
 
 });
 
-var fireURL = 'https://data.seattle.gov/resource/grwu-wqtk.json';
-var policeURL = 'https://data.seattle.gov/resource/pu5n-trf4.json';
-var testData;
-function getTestData() {
-    return $.ajax({
-        url: "https://data.seattle.gov/resource/pu5n-trf4.json?$limit=100&$order=event_clearance_date DESC&$where=event_clearance_date > '2014-07-01'&$offset=0",
-        type: "get"
-    });
-}
-getTestData().done(function(data) {
-    // Updates the UI based the ajax result
-    testData = data;
-    var i = 0;
-    testData.forEach(function(item){
-        item.key = i++;
-    });
-    React.render(<App events={ testData }/>, document.getElementById('mount'));
-});
+React.render(<App />, document.getElementById('mount'));
